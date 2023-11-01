82a095727fcbc13947517556d548b4c3d65b5efe
martin <mart337i@gmail.com>
2023-11-01 20:26:51 +0100

    [ADD] add protoype code

--- embed_esp32/src/main.cpp
+++ embed_esp32/src/main.cpp
@@ -1,138 +1,170 @@
-#include <ArduinoJson.h>
+#include <Arduino.h>
+#include <WiFi.h>
 #include <HTTPClient.h>
 #include <M5Stack.h>
-#include <WiFi.h>
+#include <time.h>
 
-const char* base_url = "http://meo.local";
+const char* ssid = "2.4ghz_meo_wifi";
+const char* password = "adminadmin";
+
+const char* BASE_URL = "http://meo.local";
+
 
 class Sensor {
 public:
-  String name;
-  String building_id;
-  String facility_id;
-  int sensor_id;
+    String name;
+    String building_id;
+    String facility_id;
+    String sensor_id;
 
-  Sensor(const char* name = "", int building_id = 1, int facility_id = 1, int sensor_id = -1) {
-    HTTPClient http;
-
-    http.begin(String(base_url) + "/get_target_name/");
-    int httpCode = http.GET();
-    if(httpCode == 200) {
-      this->name = http.getString(); // Use this-> to refer to the class member, not the parameter
+    Sensor() {
+        name = httpGet(String(BASE_URL) + "/get_target_name/");
+        building_id = httpGet(String(BASE_URL) + "/get_target_building/");
+        facility_id = httpGet(String(BASE_URL) + "/get_target_facility/");
+        
+        String url = String(BASE_URL) + "/sensor/";
+        String payload = "{\"name\":\"" + name + "\", \"building_id\":\"" + building_id + "\"}";
+        
+        String response = httpPost(url, payload);
+        if (response.indexOf("id") != -1) {
+            sensor_id = parseJson(response, "id");
+        } else {
+            sensor_id = "";
+        }
     }
-    http.end();
 
-  // For building_id
-    http.begin(String(base_url) + "/get_target_building/");
-    int httpCode = http.GET();
-    if(httpCode == 200) {
-      this->building_id = http.getString(); // Use this-> to refer to the class member
+String getCurrentDatetime() {
+    // Configure NTP
+    configTime(0, 0, "pool.ntp.org");  // UTC
+
+    time_t now = 0;
+    struct tm timeinfo = { 0 };
+    int retries = 10;
+    while(timeinfo.tm_year < (2016 - 1900) && retries--) {
+        delay(1000);
+        time(&now);
+        localtime_r(&now, &timeinfo);
     }
-    http.end();
 
-    // For facility_id
-    http.begin(String(base_url) + "/get_target_facility/");
-    httpCode = http.GET();
-    if(httpCode == 200) {
-      this->facility_id = http.getString(); // Use this-> to refer to the class member
+    char datetime[64];
+    strftime(datetime, sizeof(datetime), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
+
+    return String(datetime);
+}
+
+private:
+
+    String httpGet(const String& url) {
+        HTTPClient http;
+        http.begin(url);
+        int httpCode = http.GET();
+        if (httpCode > 0) {
+            return http.getString();
+        } else {
+            return "";
+        }
+        http.end();
     }
-    http.end();
-  }
 
-    StaticJsonDocument<200> doc;
-    doc["name"] = name;
-    doc["building_id"] = building_id;
-
-    String sensor_data;
-    serializeJson(doc, sensor_data);
-    
-    http.begin(String(base_url) + "/sensor/");
-    http.addHeader("Content-Type", "application/json");
-    httpCode = http.POST(sensor_data);
-    if(httpCode == 200) {
-      String payload = http.getString();
-      deserializeJson(doc, payload);
-      sensor_id = doc["id"];
-    } else {
-      sensor_id = -1;
+    String httpPost(const String& url, const String& payload) {
+        HTTPClient http;
+        http.begin(url);
+        http.addHeader("Content-Type", "application/json");
+        int httpCode = http.POST(payload);
+        if (httpCode > 0) {
+            return http.getString();
+        } else {
+            return "";
+        }
+        http.end();
     }
-    http.end();
-  }
+
+    String parseJson(const String& jsonData, const String& key) {
+        // Simplified JSON parsing, consider using a library for complex cases
+        String searchStr = "\"" + key + "\":\"";
+        int startPos = jsonData.indexOf(searchStr) + searchStr.length();
+        int endPos = jsonData.indexOf("\"", startPos);
+        return jsonData.substring(startPos, endPos);
+    }
 };
 
-class SensorValue {
-public:
-  int id;
-  String type;
-  float max_value;
-  float low_value;
-  float value;
-  String datetime;
-  int sensor_id;
+enum SensorType {
+    TEMPERATURE,
+    HUMIDITY
+};
+
+struct SensorValue {
+    SensorType type;
+    float max_value;
+    float low_value;
+    float value;
+    String datetime;
+    String sensor_id;
 };
 
 Sensor global_sensor;
 
-void send_alarm(String message) {
-  HTTPClient http;
-  StaticJsonDocument<200> doc;
-  doc["type"] = "failure";
-  doc["message"] = message;
-  doc["sensor_id"] = global_sensor.sensor_id;
-  
-  String data;
-  serializeJson(doc, data);
-  
-  http.begin(String(base_url) + "/alarm/");
-  http.addHeader("Content-Type", "application/json");
-  int httpCode = http.POST(data);
-  if(httpCode != 200) {
-    M5.Lcd.printf("Alarm sent: %d\n", httpCode);
-  } else {
-    M5.Lcd.printf("Alarm sent: %d\n", httpCode);
-  }
-  http.end();
-}
-
 void setup() {
-  // Initialize the M5Stack object
-  M5.begin();
-
-  // Connect to WiFi
-  // WiFi.begin(ssid, password);
-
-  // Wait for connection
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(500);
-  //   Serial.print(".");
-  // }
-
-  // Initialize global_sensor here
-  // global_sensor = ...
-
-  // Clear the screen and set up the initial display
-  M5.Lcd.clear();
-  M5.Lcd.println("Starting application");
+    M5.begin();
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+    }
+    M5.Lcd.clear();
+    M5.Lcd.println("Starting application");
 }
 
 void loop() {
-  // Main application loop
-  if (M5.BtnA.wasPressed()) {
-    // Call function to post temperature and humidity data
-    // post_temp_humidity_data();
-  }
-
-  // Update screen with sensor data
-  // ...
-
-  // Delay between readings
-  delay(1000);
+    post_temp_humidity_data();
+    M5.update();
+    delay(1000);
 }
 
-int main() {
-  setup();
-  while (true) {
-    loop();
-  }
-  return 0;
+void post_temp_humidity_data() {
+    // Placeholder for temperature and humidity readings
+    float temperature = 25.0; 
+    float humidity = 60.0;
+
+    M5.Lcd.printf("Temp: %f\nHum: %f", temperature, humidity);
+    
+    // Create SensorValue instances for temperature and humidity
+    SensorValue tempValue;
+    tempValue.type = TEMPERATURE;
+    tempValue.value = temperature;
+    tempValue.max_value = 30.0; // Placeholder max value
+    tempValue.low_value = 20.0; // Placeholder low value
+    tempValue.datetime = getCurrentDatetime(); // Placeholder datetime, consider using a real-time clock or NTP
+    tempValue.sensor_id = global_sensor.sensor_id;
+
+    SensorValue humValue;
+    humValue.type = HUMIDITY;
+    humValue.value = humidity;
+    humValue.max_value = 100.0; // Placeholder max value
+    humValue.low_value = 0.0;   // Placeholder low value
+    humValue.datetime = getCurrentDatetime();  // Placeholder datetime
+    humValue.sensor_id = global_sensor.sensor_id;
+
+    // Convert SensorValue to JSON and send using HTTP POST
+    sendSensorData(tempValue);
+    sendSensorData(humValue);
+}
+
+void sendSensorData(const SensorValue& data) {
+    String typeStr = (data.type == TEMPERATURE) ? "TEMPERATURE" : "HUMIDITY";
+    String payload = "{\"type\":\"" + typeStr + "\",";
+    payload += "\"value\":" + String(data.value) + ",";
+    payload += "\"max_value\":" + String(data.max_value) + ",";
+    payload += "\"low_value\":" + String(data.low_value) + ",";
+    payload += "\"datetime\":\"" + data.datetime + "\",";
+    payload += "\"sensor_id\":\"" + data.sensor_id + "\"}";
+
+    HTTPClient http;
+    http.begin(String(BASE_URL) + "/send_sensor_data/"); // Assuming this endpoint
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST(payload);
+    if (httpCode > 0) {
+        String response = http.getString();
+        // Optionally handle the response, e.g., check for success
+    }
+    http.end();
 }
